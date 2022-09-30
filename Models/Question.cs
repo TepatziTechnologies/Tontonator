1e2e89dd85b2023e27d7b2e0f8b0caa113ad1992
RodriiTepatzi <rodrif19@hotmail.com>
2022-09-30 11:34:20 -0500

    Creating Question class

---
+++ Models/Question.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tontonator.Models
+{
+	internal class Question
+	{
+		public string QuestionName { get; set; }
+		public string QuestionCategory { get; set; }
+		public string[] QuestionOptions { get; set; }
+
+		public Question(string questionName, string questionCategory, string[] questionOptions)
+		{
+			this.QuestionName = questionName;
+			this.QuestionCategory = questionCategory;
+			this.QuestionOptions = questionOptions;
+		}
+	}
+}
